8984e1e571fa9fe79a3ea3303d07c0902e9a237d
LaDonna Moaney <ladonnamoney@gmail.com>
2019-04-24 22:13:26 -0400

    added solution to the refactor page, made classes, did not finish adding the humanoids

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,43 @@
 // CODE here for your Lambda Classes
+class GameObj {
+    constructor(attr) {
+        this.createdAt = attr.creaetdAt;
+        this.dimensions = attr.dimensions;
+        this.name = attr.name;
+    }
+
+    destroy() {
+        return `${this.name} wass removed from the game.`;
+    }
+}
+console.log(GameObj);
+console.log(destroy());
+
+class CharacterStats extends GameObj {
+    constructor(attr) {
+        super(attr);
+        this.healthpoints = attr.healthpoints;
+    }
+takeDamage() {
+    return `${this.name} took damage.`;
+    }
+}
+
+console.log(CharacterStats);
+console.log(takeDamage());
+
+class Humanoid extends GameObj {
+    constructor(attr) {
+    super(attr);
+    this.team = attr.team;
+    this.weapons = attr.weapons;
+    this.language = attr.language;
+    }
+
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+}
+
+console.log(Humanoid);
+console.log(greet());
